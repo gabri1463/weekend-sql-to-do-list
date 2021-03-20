31047e939c37415153a8c6e2f5cf0041f72e3feb
“gabri1463” <74149109+gabri1463@users.noreply.github.com>
2021-03-19 21:19:11 -0500

    added bootstrap styling to tables and buttons, added svg trashcan image

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -50,18 +50,24 @@ function displayTasks( array ) {
     let el = $( '#tasksOut' );
     el.empty();
     for( let i = 0; i < array.length; i++) {
-        let completed = `<button class="completeTaskButton" data-id="${array[i].id}">Complete</button>`
+        let completed = `<input style="width:30px;height:30px" class="form-check-input completeTaskButton" type="checkbox" data-id="${array[i].id}">`
         // if array[i].completed is true, do not append Complete button
         if( array[i].completed ) {
-            completed = '';
+            completed = `<input style="width:30px;height:30px" class="form-check-input completeTaskButton" type="checkbox" data-id="${array[i].id}" checked disabled>`;
         }
         el.append(
             `
             <tr>
                 <td>${array[i].task}</td>
-                <td>${array[i].completed}</td>
                 <td>${completed}</td>
-                <td><button class="deleteTaskButton" data-id="${array[i].id}">Delete</button></td>
+                <td>
+                    <button class="btn btn-danger deleteTaskButton" data-id="${array[i].id}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                    </button>
+                </td>
             </tr>
             `
         );
